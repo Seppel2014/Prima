53b8a9fa101e4899ffda132ee4362ccd201230bd
Seppel2014 <daiber-simon@gmx.de>
2023-04-23 20:49:41 +0200

    improved audio, added minecraft, removed stuff from steckbrief

--- Minecraft/Script/Build/Script.js
+++ Minecraft/Script/Build/Script.js
@@ -38,19 +38,19 @@ var Script;
 })(Script || (Script = {}));
 var Script;
 (function (Script) {
-    var ƒ = FudgeCore;
-    ƒ.Debug.info("Main Program Template running!");
+    var f = FudgeCore;
+    f.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
-        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
+        f.Loop.addEventListener("loopFrame" /* f.EVENT.LOOP_FRAME */, update);
         // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
         viewport.draw();
-        ƒ.AudioManager.default.update();
+        f.AudioManager.default.update();
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
