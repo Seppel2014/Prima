d7b379653c2088e605fbe7a17769ec8cd5d0619e
Seppel2014 <daiber-simon@gmx.de>
2024-02-22 13:38:04 +0100

    vampire game

---
+++ Vampire/Script/Source/Main.ts
@@ -0,0 +1,169 @@
+namespace Script {
+  export import f = FudgeCore;
+  export import fAid = FudgeAid;
+
+  export let vampire: Vampire;
+  
+  //general Variables
+  export let viewport: f.Viewport;
+  export let timeFrame: number;
+  export let ui: UI;
+  export let timer: f.Timer;
+
+  //audio variables
+  export let sounds: f.ComponentAudio[];
+  let audioListener: f.ComponentAudioListener;
+
+  //externalData
+  interface ExternalData {
+    [name: string]: number;
+  }
+  let config: ExternalData;
+
+  export let ammountLives: number;
+  export let maxTime: number;
+  export let trapsVisibility: number;
+  export let maxDarkness: number;
+  export let lightBoost: number;
+  export let trapsTriggered: number = 0;
+
+
+  let light: f.ComponentLight;
+
+  document.addEventListener("interactiveViewportStarted", <EventListener><unknown>start);
+
+  async function start(_event: CustomEvent): Promise<void> {
+    await getExternalData();
+
+    viewport = _event.detail;
+
+    light = viewport.getBranch().getChildrenByName("AmbientLight")[0].getComponent(f.ComponentLight)
+    light.light.color = new f.Color(0.05,0.05,0.05)
+
+    vampire = new Vampire();
+    let cmpCamera: f.ComponentCamera = viewport.getBranch().getChildrenByName("Vampire")[0].getComponent(f.ComponentCamera);
+    viewport.camera = cmpCamera;
+
+    timer = new f.Timer(new f.Time, 1000, 0, updateTimer);
+    ui = new UI(maxTime);
+    
+    sounds= viewport.getBranch().getComponents(f.ComponentAudio);
+    sounds[0].play(true);
+    audioListener = viewport.getBranch().getComponent(f.ComponentAudioListener);
+    f.AudioManager.default.listenWith(audioListener);
+    f.AudioManager.default.listenTo(viewport.getBranch());
+
+    createTraps();
+    createMachines();
+
+    f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
+    f.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+  }
+
+  function update(_event: Event): void {
+    timeFrame = f.Loop.timeFrameGame / 1000; // time since last frame in seconds
+    f.Physics.simulate();  // if physics is included and used
+    
+    vampire.move();
+    changeLight(0);
+
+    viewport.draw();
+    f.AudioManager.default.update();
+  }
+
+  export function changeLight(_value:number):void {
+    let color:f.Color = light.light.color;
+    let change:number = color.r+0.001+_value
+    //console.log((1+_value-(f.Time.game.get()/1000)/maxTime))
+
+    if(change > maxDarkness) {
+      let reducedColor: f.Color = new f.Color(change,change,change);
+      light.light.color = reducedColor
+    }
+  }
+
+  async function getExternalData(): Promise<void> {
+    let response: Response = await fetch("Script/config.json");
+    config = await response.json();
+
+    ammountLives = config["lives"];
+    maxTime = config["maxTime"];
+    trapsVisibility = config["trapsVisibility"]
+    maxDarkness = config["maxDarkness"]
+    lightBoost = config["lightboost"]
+  }
+
+  function createTraps():void {
+    let TrapsPlaceholders = viewport.getBranch().getChildrenByName("TrapPlaceholders")[0].getChildren();
+    for (let trapP of TrapsPlaceholders) {
+      new Trap(trapP.getComponent(f.ComponentMesh).mtxPivot.translation,trapP.name)
+    }
+  }
+
+  function createMachines():void {
+    let StateMachines = viewport.getBranch().getChildrenByName("StateMachines")[0].getChildren();
+    for (let machine of StateMachines) {
+      new Light(machine.name)
+    }
+  }
+
+  function updateTimer():void {
+    ui.time-=1;
+    if (ui.time < 0) {
+      EndGame("time");
+    }
+  }
+
+  export function EndGame(_endReason: string): void {
+    ui.ui.style.visibility = "hidden";
+    ui.time = 10000
+    
+    let endDiv = document.getElementById("endDiv");
+    endDiv.style.display="flex";
+
+    switch(_endReason) {
+      case "time": {
+        console.log("ran out of time")
+        sounds[2].play(true);
+        endDiv.innerHTML = "YOU RAN OUT OF TIME<br>You collected "+ui.coins+" coins, <br> you triggered "+ui.traps+" traps <br> You had "+ui.lives+" lives left" 
+        break;
+      }
+      case "lives": {
+        console.log("ran out of lives")
+        endDiv.innerHTML = "YOU RAN OUT OF LIVES<br>You collected "+ui.coins+" coins <br> You triggered "+ui.traps+" traps" 
+        sounds[2].play(true);
+        break;
+      }
+      case "end": {
+        console.log("reached End")
+        endDiv.innerHTML = "YOU REACHED THE END<br>You collected "+ui.coins+" coins <br> You triggered "+ui.traps+" traps <br> You had "+ui.lives+" lives left" 
+        sounds[3].play(true);
+        break;
+      }
+    }
+    f.Loop.removeEventListener(f.EVENT.LOOP_FRAME, update);
+  }
+
+  export function createCompleteMeshNode(_name: string, _material: f.Material, _mesh: f.Mesh, _mass: number, _physicsType: f.BODY_TYPE, _group: f.COLLISION_GROUP = f.COLLISION_GROUP.DEFAULT, _colType: f.COLLIDER_TYPE = f.COLLIDER_TYPE.CUBE): f.Node {
+    let node: f.Node = new f.Node(_name);
+    let cmpMesh: f.ComponentMesh = new f.ComponentMesh(_mesh);
+    let cmpMaterial: f.ComponentMaterial = new f.ComponentMaterial(_material);
+
+    let cmpTransform: f.ComponentTransform = new f.ComponentTransform();
+
+    let cmpRigidbody: f.ComponentRigidbody = new f.ComponentRigidbody(_mass, _physicsType, _colType, _group);
+    cmpRigidbody.restitution = 0.2;
+    cmpRigidbody.friction = 0.8;
+    cmpRigidbody.dampTranslation = 0
+    
+    node.addComponent(cmpMesh);
+    node.addComponent(cmpMaterial);
+
+    cmpMaterial.mtxPivot.rotate(-90)
+
+    node.addComponent(cmpTransform);
+    node.addComponent(cmpRigidbody);
+
+    return node;
+  }
+}
---
+++ Vampire/Script/Source/Vampire.ts
@@ -0,0 +1,154 @@
+namespace Script {
+    import f = FudgeCore;
+  
+    export class Vampire extends f.Node {
+    vampire: f.Node = viewport.getBranch().getChildrenByName("Vampire")[0];
+    gravity: number = -9.81;
+    ySpeed: number = 0;
+    isGrounded: boolean = true;
+    vampireStartPoint: f.Vector3 = new f.Vector3(-2,0.4,0);
+    xSpeed: number = 5;
+    
+    constructor() {
+        super("Vampire");
+        this.vampire.getComponent(f.ComponentRigidbody).addEventListener(f.EVENT_PHYSICS.COLLISION_ENTER, this.hndCollisions)
+    }
+    
+    public hndCollisions(_event: f.EventPhysics): void {
+      let parent:f.Node = _event.cmpRigidbody.node.getParent();
+      if (parent.name == "TrapsBlocks") {
+        ui.traps+=1
+      }
+      
+      if (_event.cmpRigidbody.node.name == "EndPost") {
+        EndGame("end");
+      }
+    }
+
+    public move(): void {
+        if (f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_RIGHT, f.KEYBOARD_CODE.D])) {
+            this.vampire.mtxLocal.translateX(this.xSpeed * timeFrame);
+            this.updateAnimation("runningright");
+            }
+        
+        else if(f.Keyboard.isPressedOne([f.KEYBOARD_CODE.ARROW_LEFT, f.KEYBOARD_CODE.A])) {
+            this.vampire.mtxLocal.translateX(-this.xSpeed * timeFrame);
+            this.updateAnimation("runningleft")
+            }
+       
+        else {this.updateAnimation("idle")};
+    
+        if(this.isGrounded && f.Keyboard.isPressedOne([f.KEYBOARD_CODE.SPACE])) {
+            this.ySpeed = 5;
+            this.isGrounded = false;
+            sounds[1].play(true);
+        }
+    
+        else if(!this.isGrounded) {
+            this.updateAnimation("jumping")
+        }
+    
+        this.ySpeed += this.gravity * timeFrame;
+        
+        let pos = this.vampire.mtxLocal.translation;
+        
+        pos.y += this.ySpeed * timeFrame;
+
+        let tileCollided: f.Node = this.checkCollisionWithBlock(pos);
+        if (tileCollided) {
+          this.ySpeed = 0;
+          pos.y = tileCollided.mtxWorld.translation.y + 0.45;
+          this.isGrounded = true;
+        }
+        
+        if (pos.y < -5) {
+          this.ySpeed = 0;
+          pos = this.vampireStartPoint;
+          ui.lives--;
+          sounds[2].play(true);
+    
+          if (ui.lives < 1){
+            EndGame("lives");
+          }
+        }
+    
+        this.vampire.mtxLocal.translation = pos;
+
+        this.checkCollisionWithGold(pos);
+    
+        viewport.draw();
+        f.AudioManager.default.update();
+      }
+    
+    public updateAnimation(_animation: string): void {
+        let currentAnimation = this.vampire.getChildrenByName("VampireAnimation")[0].getComponent(f.ComponentAnimator);
+        let transformMtx: f.Matrix4x4 = this.vampire.getChildrenByName("VampireAnimation")[0].getComponent(f.ComponentTransform).mtxLocal;
+        let newAnimation: f.Animation;
+    
+        switch(_animation) {
+          case "idle": {
+            newAnimation = (f.Project.getResourcesByName("animation_idle")[0]) as f.Animation
+            let newrotation = new f.Vector3(transformMtx.rotation.x, 0, transformMtx.rotation.z)
+            transformMtx.rotation = newrotation;
+            break;
+          }
+          case "runningleft": {
+            newAnimation = (f.Project.getResourcesByName("animation_running")[0]) as f.Animation
+            let newrotation = new f.Vector3(transformMtx.rotation.x, 180, transformMtx.rotation.z)
+            transformMtx.rotation = newrotation;
+            break;
+          }
+          case "runningright": {
+            newAnimation = (f.Project.getResourcesByName("animation_running")[0]) as f.Animation
+            let newrotation = new f.Vector3(transformMtx.rotation.x, 0, transformMtx.rotation.z)
+            transformMtx.rotation = newrotation;
+            break;
+          }
+          case "jumping": {
+            newAnimation = (f.Project.getResourcesByName("animation_jumping")[0]) as f.Animation
+            let newrotation = new f.Vector3(transformMtx.rotation.x, 0, transformMtx.rotation.z)
+            transformMtx.rotation = newrotation;
+            break;
+          }
+        }
+    
+        if(currentAnimation.animation !== newAnimation) {
+        currentAnimation.animation = newAnimation;
+        }
+      }
+    
+    public checkCollisionWithBlock(_posWorld: f.Vector3): f.Node {
+        let tiles: f.Node[] = viewport.getBranch().getChildrenByName("Terrain")[0].getChildren()
+        for (let tile of tiles) {
+          let pos: f.Vector3 = f.Vector3.TRANSFORMATION(_posWorld, tile.mtxWorldInverse, true);
+          if (pos.y < 0.45 && pos.x > -0.6 && pos.x < 0.6){
+            if(pos.y < -1) {
+              break;
+            }
+            else {
+              return tile;
+            }
+          }
+        }
+        return null;
+      }
+    
+    public checkCollisionWithGold(_posWorld: f.Vector3): f.Node {
+        let golds: f.Node[] = viewport.getBranch().getChildrenByName("Golds")[0].getChildren()
+        for (let gold of golds) {
+          let pos: f.Vector3 = f.Vector3.TRANSFORMATION(_posWorld, gold.mtxWorldInverse, true);
+          if (pos.y < 0.45 && pos.x > -0.6 && pos.x < 0.6){
+            if(pos.y < -1) {
+              break;
+            }
+            else {
+              viewport.getBranch().getChildrenByName("Golds")[0].removeChild(gold);
+              ui.coins++;
+              sounds[4].play(true);
+            }
+          }
+        }
+        return null;
+      }
+    }
+}
